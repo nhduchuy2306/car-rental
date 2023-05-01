9b264dd89fb4a98565388b097df0a5aacdf70436
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2023-05-01 15:52:52 +0700

    update bashscript

--- run.bash
+++ run.bash
@@ -1,1 +1,5 @@
-mvn clean install -DskipTests && java -jar target/carrental-1.0.jar
+mvn clean install -DskipTests 
+
+java -jar target/carrental-1.0.jar 
+
+git rm --cached src/main/resources/environment.yml
