8463a1bdbd8f7d1ccbe4718534fc52a0e044bbcf
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2023-05-04 15:35:00 +0700

    new setup for branch socket

--- src/main/java/com/carrental/controllers/DemoController.java
+++ src/main/java/com/carrental/controllers/DemoController.java
@@ -5,18 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello World! Deploy successful!";
-    }
-    
-    @GetMapping("/hello-2")
-    public String sayHello2(){
-        return "Hello World! Deploy successful! 2";
-    }
-    
-    @GetMapping("/hello-3")
-    public String sayHello3(){
-        return "Hello World! Deploy successful! 3";
     }
 }
