dc07c48330ba070ec69d72d98fc500106e96753d
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2023-05-05 21:41:00 +0700

    new demo project

--- src/main/java/com/carrental/exceptions/HandleNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.carrental.exceptions;
-
-public class HandleNotFound extends RuntimeException {
-    public HandleNotFound(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/carrental/services/DemoService.java
+++
@@ -1,5 +0,0 @@
-package com.carrental.services;
-
-public class DemoService {
-    
-}
